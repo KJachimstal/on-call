9bb0f30c1f274bf349e40a79b1041105460f38a4
Konrad Jachimstal <k.jachimstal@gmail.com>
2022-03-23 22:37:04 +0100

    Add prototype schedule person photo

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import photo_placeholder from '../../assets/images/Portrait_Placeholder.png';
 
 const Schedule = () => {
   return (
-    <div className="schedule">Schedule</div>
+    <div className="schedule">
+      <div className="schedule__person">
+        <img src={photo_placeholder} className="schedule__person__photo"/>
+        <div className="schedule__person__name">Karol Kowalski</div>
+        <div className="schedule__person__login">karkow</div>
+      </div>
+    </div>
   )
 }
 
