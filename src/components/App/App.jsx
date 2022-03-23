50f70188af30355e494dd551a7aab29d9874c14d
Konrad Jachimstal <k.jachimstal@gmail.com>
2022-03-23 13:11:54 +0100

    Create structure of project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import Papa from 'papaparse';
+
+import '../../assets/scss/index.scss';
 
 const App = () => {
 
   useEffect(() => {
     fetch("/data.csv").then(response => {
       // The API call was successful!
-      return response.json();
+      return response.text();
     }).then(data => {
       // This is the JSON from our response
-      console.log(data);
+      const result = Papa.parse(data, { header: true })
+      console.log(result);
     }).catch(err => {
       // There was an error
       console.warn('Something went wrong.', err);
