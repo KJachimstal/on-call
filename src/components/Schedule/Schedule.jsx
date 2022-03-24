3f756acc36601dc88b51a7ce5e8c21697eff51c3
Konrad Jachimstal <k.jachimstal@gmail.com>
2022-03-24 12:46:15 +0100

    Update schedule styles

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -4,11 +4,15 @@ import photo_placeholder from '../../assets/images/Portrait_Placeholder.png';
 const Schedule = () => {
   return (
     <div className="schedule">
-      <div className="schedule__person">
-        <img src={photo_placeholder} className="schedule__person__photo"/>
-        <div className="schedule__person__name">Karol Kowalski</div>
-        <div className="schedule__person__login">karkow</div>
-      </div>
+      {[1, 2, 3, 4, 5].map(i => (
+        <div className="schedule__person" key={i}>
+          <div className="schedule__person__container">
+            <img src={photo_placeholder} className="schedule__person__photo" />
+            <div className="schedule__person__name">Karol Kowalski</div>
+            <div className="schedule__person__login">karkow</div>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
