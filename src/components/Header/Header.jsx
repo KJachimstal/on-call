82d8184cd62b0f25a5134db15017b76cb4f2bab3
Konrad Jachimstal <k.jachimstal@gmail.com>
2022-03-25 21:14:43 +0100

    Add tabs in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import moment from 'moment';
 import Moment from 'react-moment';
 import 'moment/locale/pl';
+import { AiOutlineSchedule } from 'react-icons/ai';
 
 import logo from '../../assets/images/OpsTechIT_logo_on_BK_nosmile.png';
 
@@ -22,6 +23,10 @@ const Header = () => {
       <div className="header__time">
         <Moment date={time} locale="pl" interval={1000} format="DD MMMM YYYY, HH:mm:ss" />
       </div>
+      <div className="header__tab"><AiOutlineSchedule /></div>
+      <div className="header__tab"><AiOutlineSchedule /></div>
+      <div className="header__tab"><AiOutlineSchedule /></div>
+      <div className="header__tab"><AiOutlineSchedule /></div>
       <img src={logo} alt="ops-techIT" className="header__logo" />
     </div>
   )
