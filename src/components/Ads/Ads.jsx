69321884cafdba00dfacb58850d421d055f31bc2
Konrad Jachimstal <k.jachimstal@gmail.com>
2022-03-25 18:47:21 +0100

    Correct fit image to slide

--- src/components/Ads/Ads.jsx
+++ src/components/Ads/Ads.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
-import ad from '../../assets/images/How-to-cut-ticket-RIVER.png';
+import howToCutTicket from '../../assets/images/How-to-cut-ticket-RIVER.png';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Ads = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    // centerMode: true
+    // className: "slide"
+  };
+
   return (
     <div className="ads">
-      <img src={ad} className="ads__ad"/>
+      <div className="ads__ad">
+        <Slider {...settings}>
+          <div>
+            <img src={howToCutTicket} alt="howToCutTicket" className = "slide"/>
+          </div>
+          <div>
+          <img src={howToCutTicket} alt="howToCutTicket" className = "slide" />
+          </div>
+          <div>
+          <img src={howToCutTicket} alt="howToCutTicket" className = "slide" />
+          </div>
+        </Slider>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Ads
